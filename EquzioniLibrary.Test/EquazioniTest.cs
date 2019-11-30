0ceca0ccc65835b1e7c0e63590cd46e059e11893
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-30 11:21:28 +0100

    fine di metodi

--- EquzioniLibrary.Test/EquazioniTest.cs
+++ EquzioniLibrary.Test/EquazioniTest.cs
@@ -7,10 +7,60 @@ namespace EquzioniLibrary.Test
     [TestClass]
     public class EquazioniTest
     {
-        [TestMethod]
-        public void TestMethod1()
+                    [TestMethod]
+        public void IsDeterminedTest()
         {
-
+            double a = 3;
+            double b = 4;
+            bool risposta = Equazioni.IsDetermined(a, b);
+            Assert.AreEqual(true, risposta);
+        }
+        [TestMethod]
+        public void IsNotDeterminedTest()
+        {
+            double a = 0;
+            double b = 4;
+            bool risposta = Equazioni.IsDetermined(a, b);
+            Assert.AreEqual(false, risposta);
+        }
+        [TestMethod]
+        public void IsInconsistedTest()
+        {
+            double a = 0;
+            double b = 3;
+            bool risposta = Equazioni.IsInconsisted(a, b);
+            Assert.AreEqual(true, risposta);
+        }
+        [TestMethod]
+        public void IsNotInconsistedTest()
+        {
+            double a = 3;
+            double b = 1;
+            bool risposta = Equazioni.IsInconsisted(a, b);
+            Assert.AreEqual(false, risposta);
+        }
+        [TestMethod]
+        public void IsDegree2Test()
+        {
+            double a = 2;
+            bool risposta = Equazioni.IsDegree2(a);
+            Assert.AreEqual(true, risposta);
+        }
+        [TestMethod]
+        public void IsNotDegree2Test()
+        {
+            double a = 0;
+            bool risposta = Equazioni.IsDegree2(a);
+            Assert.AreEqual(false, risposta);
+        }
+        [TestMethod]
+        public void DeltaTest()
+        {
+            double a = 4;
+            double b = 3;
+            double c = 2;
+            double delta = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(-23, delta);
         }
     }
 }
